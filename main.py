c8ce3d08914a046dd997879b7c6b4585bb57f7a9
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-05-01 21:56:38 +0200

    Make a draft for adding offers

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import time
+import os
+import json
+
 from selenium import webdriver
 from credentials import Credentials
 from prepare_and_check_offers import PrepareAndCheckOffers
 from logging_to_sites import LoginOLX, LoginAllegroLokalnie, LoginSprzedajemy, LoginVinted
 from opening_sites import OpenOLX, OpenAllegroLokalnie, OpenSprzedajemy, OpenVinted
+from post_offers import PostOLX, PostAllegroLokalnie, PostSprzedajemy, PostVinted
+from prepare_and_check_offers import OFFERS_FOLDER_NAME
 
 LIST_OF_BROWSERS = ["chrome", "edge", "firefox", "ie"]
 LIST_OF_DRIVERS = [webdriver.Chrome, webdriver.Edge, webdriver.Firefox, webdriver.Ie]
 BROWSER_DRIVER_DICTIONARY = {browser: driver for browser, driver in zip(LIST_OF_BROWSERS, LIST_OF_DRIVERS)}
 PRIVATE_OFFER, BUSINESS_OFFER = "Prywatne", "Firmowe"
+OFFERS_FOLDER_PATH = os.path.dirname(__file__) + "/"+ OFFERS_FOLDER_NAME + "/"
 driver = None
 credentials = Credentials()
 
@@ -38,8 +43,14 @@ def post_offers_olx():
     olx = LoginOLX(driver)
     olx.login_to_page(credentials.login_olx, credentials.password_olx)
     del credentials.password_olx
-    time.sleep(3)
-    #TODO: Add posting of offers
+    post_offer_olx = PostOLX(driver)
+    list_of_offers = [offer.path for offer in os.scandir(OFFERS_FOLDER_PATH) if offer.is_dir()]
+    for offer_path in list_of_offers:
+        with open(offer_path + "/properties.json", "r") as properties_file:
+            properties_data = json.load(properties_file)
+        post_offer_olx.click_add_new_offer()
+        post_offer_olx.fill_offer_with_data(properties_data, offer_path + "/photos")
+        post_offer_olx.click_add_offer()
     olx.click_logout()
 
 def post_offers_allegro_lokalnie():
@@ -49,8 +60,14 @@ def post_offers_allegro_lokalnie():
     allegro_lokalnie = LoginAllegroLokalnie(driver)
     allegro_lokalnie.login_to_page(credentials.login_allegro_lokalnie, credentials.password_allegro_lokalnie)
     del credentials.password_allegro_lokalnie
-    time.sleep(3)
-    #TODO: Add posting of offers
+    post_offer_allegro_lokalnie = PostAllegroLokalnie(driver)
+    list_of_offers = [offer.path for offer in os.scandir(OFFERS_FOLDER_PATH) if offer.is_dir()]
+    for offer_path in list_of_offers:
+        with open(offer_path + "/properties.json", "r") as properties_file:
+            properties_data = json.load(properties_file)
+        post_offer_allegro_lokalnie.click_add_new_offer()
+        post_offer_allegro_lokalnie.fill_offer_with_data(properties_data, offer_path + "/photos")
+        post_offer_allegro_lokalnie.click_add_offer()
     allegro_lokalnie.click_logout()
 
 def post_offers_sprzedajemy():
@@ -60,8 +77,14 @@ def post_offers_sprzedajemy():
     sprzedajemy = LoginSprzedajemy(driver)
     sprzedajemy.login_to_page(credentials.login_sprzedajemy, credentials.password_sprzedajemy)
     del credentials.password_sprzedajemy
-    time.sleep(3)
-    #TODO: Add posting of offers
+    post_offer_sprzedajemy = PostSprzedajemy(driver)
+    list_of_offers = [offer.path for offer in os.scandir(OFFERS_FOLDER_PATH) if offer.is_dir()]
+    for offer_path in list_of_offers:
+        with open(offer_path + "/properties.json", "r") as properties_file:
+            properties_data = json.load(properties_file)
+        post_offer_sprzedajemy.click_add_new_offer()
+        post_offer_sprzedajemy.fill_offer_with_data(properties_data, offer_path + "/photos")
+        post_offer_sprzedajemy.click_add_offer()
     sprzedajemy.click_logout()
 
 def post_offers_vinted():
@@ -71,8 +94,14 @@ def post_offers_vinted():
     vinted = LoginVinted(driver)
     vinted.login_to_page(credentials.login_vinted, credentials.password_vinted)
     del credentials.password_vinted
-    time.sleep(3)
-    #TODO: Add posting of offers
+    post_offer_vinted = PostVinted(driver)
+    list_of_offers = [offer.path for offer in os.scandir(OFFERS_FOLDER_PATH) if offer.is_dir()]
+    for offer_path in list_of_offers:
+        with open(offer_path + "/properties.json", "r") as properties_file:
+            properties_data = json.load(properties_file)
+        post_offer_vinted.click_add_new_offer()
+        post_offer_vinted.fill_offer_with_data(properties_data, offer_path + "/photos")
+        post_offer_vinted.click_add_offer()
     vinted.click_logout()
 
 if __name__ == "__main__":
