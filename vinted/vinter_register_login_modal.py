f9b54b9cb7d3e04976ca9f590f4d0c195363e4ff
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-03 23:51:37 +0100

    Add method to click on login by email button, refactor xpaths.

--- vinted/vinter_register_login_modal.py
+++ vinted/vinter_register_login_modal.py
@@ -6,14 +6,15 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from vinted.vinted_constants import MODALS_TIMEOUT
+from vinted.vinted_login_by_email_modal import VintedLoginByEmailModal
 from vinted.vinted_register_by_email_modal import VintedRegisterByEmailModal
 
 
 class VintedRegisterLoginModal:
     modal_xpath = "//div[contains(@class, 'ReactModal__Content--after-open')]"
     x_button_xpath = "//span[@data-icon-name='x']//ancestor::button"
-    register_by_email_xpath = "//span[@data-testid='auth-select-type--register-email' or " \
-                              "@data-testid='auth-select-type--login-email']"
+    register_by_email_xpath = "//span[@data-testid='auth-select-type--register-email']"
+    login_by_email_xpath = "//span[@data-testid='auth-select-type--login-email']"
     login_register_switch_xpath = "//span[@data-testid='auth-select-type--{}-switch']"
 
     def __init__(self, driver: webdriver.Chrome):
@@ -31,6 +32,10 @@ class VintedRegisterLoginModal:
     def click_register_by_email(self) -> VintedRegisterByEmailModal:
         self.driver.find_element(by=By.XPATH, value=self.modal_xpath + self.register_by_email_xpath).click()
         return VintedRegisterByEmailModal(self.driver)
+
+    def click_login_by_email(self) -> VintedLoginByEmailModal:
+        self.driver.find_element(by=By.XPATH, value=self.modal_xpath + self.login_by_email_xpath).click()
+        return VintedLoginByEmailModal(self.driver)
 
     def switch_login_register(self, to_option: Literal["login", "register"]):
         to_option_dict = {"login": "register", "register": "login"}
