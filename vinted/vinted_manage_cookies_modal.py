9cad05850a3a7ffc8d7370028d2b83bb226e495a
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-03 22:16:37 +0100

    Remove unused import.

--- vinted/vinted_manage_cookies_modal.py
+++ vinted/vinted_manage_cookies_modal.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
