47eafb8ad41067494082a92a9a21bbf67811ab49
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-03 22:23:19 +0100

    Update wait_for_essentials method.

--- vinted/vinted_manage_cookies_modal.py
+++ vinted/vinted_manage_cookies_modal.py
@@ -22,7 +22,7 @@ class VintedManageCookiesModal:
     def wait_for_essentials(self) -> None:
         for element_xpath in [self.x_button_xpath, self.allow_all_cookies_xpath, self.confirm_my_choices_xpath]:
             WebDriverWait(self.driver, timeout=MODALS_TIMEOUT).\
-                until(EC.element_to_be_clickable((By.XPATH, element_xpath)))
+                until(EC.element_to_be_clickable((By.XPATH, self.modal_xpath + element_xpath)))
 
     def click_x_button(self) -> None:
         self.driver.find_element(by=By.XPATH, value=self.modal_xpath + self.x_button_xpath).click()
