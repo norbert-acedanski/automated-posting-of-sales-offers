e91f94ce35e3c8de12bd36a69ef3e1da678964ca
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-04 13:10:27 +0100

    Add proper setup for opening the browser.

--- vinted/open_vinted_page.py
+++ vinted/open_vinted_page.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.chrome.service import Service
+
 from vinted.vinted_constants import MAIN_PAGE_URL
 from vinted.vinted_main_page import VintedMainPage
 
@@ -5,6 +7,11 @@ from selenium import webdriver
 
 
 def open_vinted_main_page() -> VintedMainPage:
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    driver = webdriver.Chrome(service=Service("./chromedriver/chromedriver.exe"), options=chrome_options)
     driver.get(MAIN_PAGE_URL)
     return VintedMainPage(driver=driver, wait_for_essentials=False, wait_for_cookies=True)
