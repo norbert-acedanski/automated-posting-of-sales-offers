6f8d8cf51e1b5044fa06f0bd2f49c223bc662c48
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-04 21:32:14 +0100

    Test all methods for VintedRegisterLoginModal class and change failing ones.

--- vinted/vinter_register_login_modal.py
+++ vinted/vinter_register_login_modal.py
@@ -13,6 +13,9 @@ from vinted.vinted_register_by_email_modal import VintedRegisterByEmailModal
 class VintedRegisterLoginModal:
     modal_xpath = "//div[contains(@class, 'ReactModal__Content--after-open')]"
     x_button_xpath = "//span[@data-icon-name='x']//ancestor::button"
+    continue_with_facebook_button_xpath = "//span[@data-icon-name='signup/facebook']/ancestor::button"
+    continue_with_google_button_xpath = "//span[@data-icon-name='signup/google']/ancestor::button"
+    continue_with_apple_button_xpath = "//span[@data-icon-name='signup/apple']/ancestor::button"
     register_by_email_xpath = "//span[@data-testid='auth-select-type--register-email']"
     login_by_email_xpath = "//span[@data-testid='auth-select-type--login-email']"
     login_register_switch_xpath = "//span[@data-testid='auth-select-type--{}-switch']"
@@ -22,7 +25,9 @@ class VintedRegisterLoginModal:
         self.wait_for_essentials()
 
     def wait_for_essentials(self, timeout: Union[float, int] = MODALS_TIMEOUT) -> None:
-        for element_xpath in [self.x_button_xpath, self.register_by_email_xpath, self.login_register_switch_xpath]:
+        for element_xpath in [self.x_button_xpath, self.continue_with_facebook_button_xpath,
+                              self.continue_with_google_button_xpath, self.continue_with_apple_button_xpath,
+                              self.register_by_email_xpath, self.login_register_switch_xpath.format("register")]:
             WebDriverWait(self.driver, timeout=timeout).\
                 until(EC.element_to_be_clickable((By.XPATH, self.modal_xpath + element_xpath)))
 
@@ -40,5 +45,5 @@ class VintedRegisterLoginModal:
     def switch_login_register(self, to_option: Literal["login", "register"]):
         to_option_dict = {"login": "register", "register": "login"}
         element_xpath = self.modal_xpath + self.login_register_switch_xpath.format(to_option_dict[to_option])
-        if self.driver.find_element(by=By.XPATH, value=element_xpath).is_displayed():
+        if self.driver.find_elements(by=By.XPATH, value=element_xpath):
             self.driver.find_element(by=By.XPATH, value=element_xpath).click()
