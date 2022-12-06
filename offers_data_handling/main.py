3b74cd4b53b8d630379c1551dcf819644795edc3
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-06 22:49:38 +0100

    Change name of example offer to get data.

--- offers_data_handling/main.py
+++ offers_data_handling/main.py
@@ -3,4 +3,4 @@ from offers_data_handling.offers_data_collector import OffersDataCollector
 if __name__ == "__main__":
     offers_data_collector = OffersDataCollector()
     print(offers_data_collector.get_offers_names())
-    print(offers_data_collector.get_offer_properties("Sample offer 1"))
+    print(offers_data_collector.get_offer_properties("Offer example structure"))
