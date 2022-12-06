6aa9566de2620fa36caa08f6a31bb8c801d33a77
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-06 22:16:57 +0100

    Replace backslash with forward slash.

--- common/common.py
+++ common/common.py
@@ -1,1 +1,1 @@
-OFFERS_FOLDER_PATH = "\\offers_to_post"
+OFFERS_FOLDER_PATH = "/offers_to_post"
--- offers_data_handling/offers_data_handler.py
+++ offers_data_handling/offers_data_handler.py
@@ -13,12 +13,12 @@ class OffersDataHandler:
         return [offer.name for offer in os.scandir(self.global_offers_path) if offer.is_dir()]
 
     def get_offer_properties(self, offer_name: str) -> Dict[str, Union[List[str], str, Dict[str, str]]]:
-        offer_path = f"{self.global_offers_path}\\{offer_name}"
-        photos_path = f"{offer_path}\\photos"
+        offer_path = f"{self.global_offers_path}/{offer_name}"
+        photos_path = f"{offer_path}/photos"
         offer_properties = {"title": offer_name,
-                            "photos": [f"{photos_path}\\{photo.name}" for photo in os.scandir(photos_path)
+                            "photos": [f"{photos_path}/{photo.name}" for photo in os.scandir(photos_path)
                                        if not photo.is_dir()]}
-        with open(f"{offer_path}\\properties.json", "r", encoding="utf-8") as properties_file:
+        with open(f"{offer_path}/properties.json", "r", encoding="utf-8") as properties_file:
             properties_data = json.load(properties_file)
         offer_properties.update(properties_data)
         return offer_properties
