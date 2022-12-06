90f4741865799451a83315f4ae95b21f1ad359e7
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-06 23:21:40 +0100

    Refactor methods names.

--- offers_info_for_pages/main.py
+++ offers_info_for_pages/main.py
@@ -2,9 +2,9 @@ from offers_info_for_pages import OffersInfoForPages
 
 if __name__ == "__main__":
     offers_info = OffersInfoForPages()
-    offers_info.print_bounds_for_title()
-    offers_info.print_bounds_for_description()
-    offers_info.print_photos_restrictions()
-    offers_info.print_conditions_restrictions()
-    offers_info.print_sexes_restrictions()
-    offers_info.print_package_size_restrictions()
+    offers_info.print_limits_for_title()
+    offers_info.print_limits_for_description()
+    offers_info.print_limits_for_photos()
+    offers_info.print_limits_for_restrictions()
+    offers_info.print_limits_for_sexes()
+    offers_info.print_limits_for_package_sizes()
--- offers_info_for_pages/offers_info_for_pages.py
+++ offers_info_for_pages/offers_info_for_pages.py
@@ -13,7 +13,7 @@ class OffersInfoForPages:
         self.conditions = ["fine", "good", "very good", "new without a tag", "new with a tag"]
         self.package_sizes = ["S", "M", "L"]
 
-    def print_bounds_for_title(self):
+    def print_limits_for_title(self):
         print(FC.GREEN + "Title limits [characters]:" + FC.RESET)
         for key, value in self.title_boundaries.items():
             print(FC.YELLOW + key + FC.RESET + ": min - " + FC.BLUE + str(value["min"]) + FC.RESET + ", max - " +
@@ -21,7 +21,7 @@ class OffersInfoForPages:
         print("Valid for: 06.12.2022")
         print("")
 
-    def print_bounds_for_description(self):
+    def print_limits_for_description(self):
         print(FC.GREEN + "Description limits [characters]:" + FC.RESET)
         for key, value in self.description_boundaries.items():
             print(FC.YELLOW + key + FC.RESET + ": min - " + FC.BLUE + str(value["min"]) + FC.RESET + ", max - " +
@@ -29,7 +29,7 @@ class OffersInfoForPages:
         print("Valid for: 06.12.2022")
         print("")
 
-    def print_photos_restrictions(self):
+    def print_limits_for_photos(self):
         print(FC.GREEN + "Number of photos limits:" + FC.RESET)
         for key, value in self.number_of_photos_boundaries.items():
             print(FC.YELLOW + key + FC.RESET + ": min - " + FC.BLUE + str(value["min"]) + FC.RESET + ", max - " +
@@ -37,19 +37,19 @@ class OffersInfoForPages:
         print("Valid for: 06.12.2022")
         print("")
 
-    def print_conditions_restrictions(self):
+    def print_limits_for_restrictions(self):
         print(FC.GREEN + "Acceptable list of conditions:" + FC.RESET)
         for condition in self.conditions:
             print("* " + FC.BLUE + condition + FC.RESET)
         print("")
 
-    def print_sexes_restrictions(self):
+    def print_limits_for_sexes(self):
         print(FC.GREEN + "Acceptable sexes marks:" + FC.RESET)
         for sex in self.sexes:
             print("* " + FC.BLUE + sex + FC.RESET)
         print("")
 
-    def print_package_size_restrictions(self):
+    def print_limits_for_package_sizes(self):
         print(FC.GREEN + "Acceptable sizes of the package:" + FC.RESET)
         for package_size in self.package_sizes:
             print("* " + FC.BLUE + package_size + FC.RESET)
