f6dd1321ce82c406cb7884e8676a5c5515fe80e6
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-14 22:36:03 +0100

    Add example usage of CheckOffersForSites class methods.

---
+++ check_offers/main.py
@@ -0,0 +1,11 @@
+from check_offers import CheckOffersForSites
+
+if __name__ == "__main__":
+    offers_checker = CheckOffersForSites()
+    offers_checker.check_title_lengths()
+    offers_checker.check_description_lengths()
+    offers_checker.check_number_of_photos()
+    offers_checker.check_sexes()
+    offers_checker.check_prices()
+    offers_checker.check_conditions()
+    offers_checker.check_package_sizes()
