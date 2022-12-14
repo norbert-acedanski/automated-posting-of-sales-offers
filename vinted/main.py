aed6ecef1a650273cec6bc46b6a3724b17222893
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-14 23:34:50 +0100

    Update main.py to use OffersDataCollector.

--- vinted/main.py
+++ vinted/main.py
@@ -1,3 +1,4 @@
+from offers_data_handling.offers_data_collector import OffersDataCollector
 from open_vinted_page import open_vinted_main_page
 from vinted_main_page import VintedMainPage
 
@@ -12,16 +13,20 @@ if __name__ == "__main__":
     vinted_login_modal.fill_password(password="Sample Password")
     vinted_login_modal.click_continue_button()
     vinted_main_page.wait_for_essentials()
-    vinted_sell_page = vinted_main_page.click_sell_button()
-    photos_paths = []  # With an empty list, a trash offer will not be posted
-    vinted_sell_page.add_photos_to_offer(photos_paths)
-    vinted_sell_page.add_title_to_offer("Sample title")
-    vinted_sell_page.add_description_to_offer("Small description")
-    vinted_sell_page.choose_category(["Kobiety", "Ubrania", "Okrycia wierzchnie", "Peleryny i poncza"])
-    vinted_sell_page.choose_brand("Sample")
-    vinted_sell_page.choose_size("XS")
-    vinted_sell_page.choose_status("new without tag")
-    vinted_sell_page.choose_colors(["Czarny", "Beżowy"])
-    vinted_sell_page.add_price(100)
-    vinted_sell_page.choose_package_size("M")
-    vinted_sell_page.click_add_item_button()
+    data_collector = OffersDataCollector()
+    offers_names = data_collector.get_offers_names()
+    for offer_name in offers_names:
+        vinted_sell_page = vinted_main_page.click_sell_button()
+        current_offer_properties = data_collector.get_offer_properties(offer_name)
+        vinted_sell_page.add_photos_to_offer(current_offer_properties["photos"])
+        vinted_sell_page.add_title_to_offer(current_offer_properties["title"])
+        vinted_sell_page.add_description_to_offer(current_offer_properties["description"])
+        vinted_sell_page.choose_category(current_offer_properties["category"]["vinted"])
+        vinted_sell_page.choose_brand(current_offer_properties["brand"])
+        vinted_sell_page.choose_size(current_offer_properties["size"])
+        vinted_sell_page.choose_condition(current_offer_properties["condition"])
+        vinted_sell_page.choose_colors(current_offer_properties["colors"])
+        vinted_sell_page.add_price(current_offer_properties["price"])
+        vinted_sell_page.choose_package_size(current_offer_properties["package size"])
+        breakpoint()
+        vinted_sell_page.click_add_item_button()
